668dd14cd00ad51a44d927d7c4732f4d4ba749c1
maymyatmon31 <mmm.maymyatmon31@gmail.com>
2023-10-24 15:21:47 +0630

    fixed design

--- resources/views/layouts/user_navbar.blade.php
+++ resources/views/layouts/user_navbar.blade.php
@@ -1,8 +1,8 @@
 <div class="container position-sticky z-index-sticky top-0">
   <div class="row">
     <div class="col-12">
-      <nav class="navbar navbar-expand-lg  blur blur-rounded top-0 z-index-fixed shadow position-absolute my-3 py-2 start-0 end-0 mx-4">
-        <div class="container-fluid px-0">
+      <nav class="navbar navbar-expand-lg  blur blur-rounded  top-0 z-index-fixed shadow position-absolute my-3 py-2 start-0 end-0 mx-4">
+        <div class="container-fluid d-flex justify-content-between   px-0">
           <a class="navbar-brand font-weight-bolder text-primary text-gradient-primary ms-sm-3" href="{{ url('/') }}" rel="tooltip" title="Designed and Coded by Creative Tim" data-placement="bottom">
             Delight 2D | 3D System
           </a>
@@ -13,8 +13,8 @@
               <span class="navbar-toggler-bar bar3"></span>
             </span>
           </button>
-          <div class="collapse navbar-collapse pt-3 pb-2 py-lg-0 w-100" id="navigation">
-            <ul class="navbar-nav navbar-nav-hover ms-lg-12 ps-lg-5 w-100">
+          <div class="collapse navbar-collapse pt-3 pb-2 py-lg-0" id="navigation">
+            <ul class="navbar-nav navbar-nav-hover ps-lg-5 w-100">
 
               <li class="nav-item dropdown dropdown-hover mx-2">
                 <a class="nav-link ps-2 d-flex justify-content-between cursor-pointer align-items-center" href="{{ url('/lottery_result') }}" id="dropdownMenuBlocks">
@@ -46,18 +46,18 @@
                   Account
                   <img src="{{ asset('user_app/assets/img/down-arrow-dark.svg') }}" alt="down-arrow" class="arrow ms-1">
                 </a>
-                <div class="dropdown-menu dropdown-menu-animation dropdown-md p-3 border-radius-lg mt-0 mt-lg-3" aria-labelledby="dropdownMenuPages5">
+                <div class="dropdown-menu dropdown-menu-animation p-3 border-radius-lg mt-0 mt-lg-3" aria-labelledby="dropdownMenuPages5">
                   <div class="d-none d-lg-block">
 
                     @if(Auth::check()) <!-- If the user is logged in -->
 
                     <a href="{{ route('home') }}" class="dropdown-item border-radius-md">
-                      <span class="ps-3">Profile</span>
+                      <span>Profile</span>
                     </a>
 
                     <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="dropdown-item border-radius-md">
                       {{-- <span class="sidenav-mini-icon"> L </span> --}}
-                      <span class="sidenav-normal ms-2 ps-1">Logout</span>
+                      <span class="sidenav-normal">Logout</span>
                     </a>
                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                       @csrf
